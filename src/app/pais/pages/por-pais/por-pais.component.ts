724b5e978d49e1f878d848bd9b91bf5d0f3a1225
unknown <camiloalexis20@gmail.com>
2023-03-21 18:57:23 -0300

    Prueba

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styleUrls: ['./por-pais.component.css']
+})
+export class PorPaisComponent {
+  termino: string = '';
+
+  constructor( private paisService: PaisService) {}
+
+  buscar(){
+    console.log(this.termino);
+
+    this.paisService.buscarPais(this.termino).subscribe(resp => {
+      console.log(resp);
+    });
+
+  }
+
+}
---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import { style } from '@angular/animations';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+  
+})
+export class SidebarComponent {
+
+}
